7b3356d01ee4f94318de358fa85d16b839b71f27
Dragoș Matteo <dragosmatteod@gmail.com>
2022-08-29 17:44:34 +0300

    first commit

---
+++ feedback.php
@@ -0,0 +1,34 @@
+<?php include './inc/header.php'; ?>
+<?php
+
+$sql = 'SELECT * FROM feedback';
+$result = mysqli_query($conn, $sql);
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
+
+<h2>Past Feedback</h2>
+
+<?php
+if (empty($feedback)) : ?>
+  <p class="lead m3">There is no feedback</p>
+<?php endif; ?>
+
+
+<?php foreach ($feedback as $item) : ?>
+  <div class="card my-3 w-75">
+    <div class="card-body text-center">
+      <?= $item['body']; ?>
+
+      <div class="text-secondary mt-2">
+        By <?= $item['name'] ?> on <?= $item['date']; ?>
+      </div>
+    </div>
+  </div>
+
+<?php endforeach; ?>
+
+
+</main>
+
+<?php include './inc/footer.php'; ?>
